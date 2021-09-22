a3015da426e5f3d6de0036c92c517ac599fe0b72
adith <adith@adiths-Mac-mini.local>
2021-09-22 14:28:26 +0530

    Searching

--- src/Searching/BinarySearchRecursive.java
+++ src/Searching/BinarySearchRecursive.java
@@ -1,5 +1,22 @@
 package Searching;
 
+
+/**
+ *  Implementation:
+ *  arr[] = {10,20,30,40,50,60,70}, x=20; n=6;
+ *  formula: (low+high)/2
+ *  	mid point: 0+6/2 =>3
+ * 	bSearch (arr,0,6,20)
+ * 		mid = 0+6/2 = 3
+ * 	if(arr[mid] > x) : high = mid-1, low=0
+ * 	bSearch (arr,0,2,20)
+ * 		mid=0+2/2 = 1
+ * check if(arr[mid]==x)
+ * 		return mid
+ * 	else if(arr[mid] < x) : high = 0, low=mid+1 (element found on the right hand side of the middle element)
+ *
+ */
+
 public class BinarySearchRecursive {
 	static int bSearch(int arr[], int low, int high, int x)
 	{
@@ -28,3 +45,28 @@ public class BinarySearchRecursive {
     } 
 
 }
+
+
+/**
+ *  Implementation 2:
+ * 
+ *  arr[] = {10,20,30,40,50,60,70}, x=25; n=6;
+ *  formula: (low+high)/2
+ *  
+ * 	bSearch (arr,0,6,20)
+ * 		mid = 0+6/2 = 3						-> 40 > 25
+ * 		arr[mid] > x										
+ * 		
+ * 		bSearch(arr,0,2,25)					-> high = mid-1 and low =0	
+ * 			mid = 0+2/2 =1					
+ * 			arr[mid] < x 					-> 20 < 25; low = mid+1, high =2
+ * 			bSearch(arr,2,2,25)
+ * 				mid=2+2/2=2
+ * 				arr[mid] > x				-> 30 > 25; low=2, high=mid-1
+ * 				bSearch(arr,2,1,25)				
+ * 
+ * 
+ * 
+ * 
+ *
+ */
